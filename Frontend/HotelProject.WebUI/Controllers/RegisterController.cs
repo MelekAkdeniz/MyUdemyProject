ca2eaa8e929c77967ef26565d95708bbaec4e727
MelekAkdeniz <mnur.akdeniz@gmail.com>
2025-07-29 21:40:37 +0300

    Add user auth, registration, and make City nullable
    Introduced user authentication and registration functionality:
    - Made `City` property in `AppUser` nullable and updated schema.
    - Added `LoginController` and `RegisterController` for auth flows.
    - Created `LoginUserDto` and `CreateNewUserDto` with validation.
    - Added login and registration views with form validation.
    - Configured AutoMapper for DTO-to-entity mappings.
    - Updated project references and services for Identity and DbContext.

---
+++ Frontend/HotelProject.WebUI/Controllers/RegisterController.cs
@@ -0,0 +1,46 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using HotelProject.WebUI.Dtos.RegisterDto;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateNewUserDto createNewUserDto)
+        {
+            if(!ModelState.IsValid)
+            {
+                // If the model state is invalid, return the view with the current model state
+                return View();
+            }
+            var appUser= new AppUser()
+            {
+                Name = createNewUserDto.Name,
+                Surname = createNewUserDto.Surname,
+                UserName = createNewUserDto.UserName,
+                Email = createNewUserDto.Mail
+            };
+            var result = await _userManager.CreateAsync(appUser, createNewUserDto.Password);
+            if(result.Succeeded)
+            {
+                // User created successfully, you can redirect or show a success message
+                return RedirectToAction("Index", "Login");
+            }
+            
+            return View();
+        }
+    }
+}
