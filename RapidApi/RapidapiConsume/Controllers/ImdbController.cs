83e11e4f6d83f4d055c44feb012c8c178fa94886
MelekAkdeniz <mnur.akdeniz@gmail.com>
2025-08-10 20:03:17 +0300

    RapidApi project added.

---
+++ RapidApi/RapidapiConsume/Controllers/ImdbController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace RapidapiConsume.Controllers
+{
+    public class ImdbController : Controller
+    {
+        public async Task< IActionResult> Index()
+        {
+            
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
+                Headers =
+    {
+        { "x-rapidapi-key", "b7d12692e3msh1f3ad94cf6bbe2ap10af52jsn8e0f11eabc88" },
+        { "x-rapidapi-host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+            }
+            return View();
+        }
+    }
+}
---
+++ RapidApi/RapidapiConsume/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RapidapiConsume.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
