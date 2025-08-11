6b843b98c62d7403411baad614c15fbc83343635
MelekAkdeniz <mnur.akdeniz@gmail.com>
2025-08-11 16:54:34 +0300

    BookingRapidApi added.

---
+++ RapidApi/RapidapiConsume/Controllers/BookingController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidapiConsume.Models;
+
+namespace RapidapiConsume.Controllers
+{
+    public class BookingController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://booking-com15.p.rapidapi.com/api/v1/hotels/searchHotels?dest_id=-1456928&search_type=CITY&arrival_date=2025-08-11&departure_date=2025-08-14&adults=2&children_age=0%2C17&room_qty=1&page_number=1&units=metric&temperature_unit=c&languagecode=en-us&currency_code=EUR&location=US"),
+                Headers =
+    {
+        { "x-rapidapi-key", "b7d12692e3msh1f3ad94cf6bbe2ap10af52jsn8e0f11eabc88" },
+        { "x-rapidapi-host", "booking-com15.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<BookingApiViewModel>(body);
+                return View(values.data.hotels
+                           .Select(h => h.property)
+                           .ToList());
+            }
+        }
+    }
+}
