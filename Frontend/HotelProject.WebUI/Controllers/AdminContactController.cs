b47529c75425e51ee6c4c21e15548c4655a2e45a
Melek Nur Akdeniz <meleknur_akdeniz@hotmail.com>
2025-08-20 18:35:47 +0300

    SendMessageController api added. CreateSendMessageDto added

--- Frontend/HotelProject.WebUI/Controllers/AdminContactController.cs
+++ Frontend/HotelProject.WebUI/Controllers/AdminContactController.cs
@@ -1,7 +1,9 @@
 ﻿using HotelProject.WebUI.Dtos.ContactDto;
+using HotelProject.WebUI.Dtos.SendMessageDto;
 using HotelProject.WebUI.Models.Staff;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace HotelProject.WebUI.Controllers
 {
@@ -23,6 +25,27 @@ namespace HotelProject.WebUI.Controllers
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<List<InboxContactDto>>(jsonData);
                 return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AddSendMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddSendMessage(CreateSendMessageDto createSendMessage)
+        {
+            createSendMessage.SenderMail="admin@gmail.com";
+            createSendMessage.SenderName="admin";
+            createSendMessage.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createSendMessage);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("http://localhost:40510/api/SendMessage", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("SendBox");
             }
             return View();
         }
