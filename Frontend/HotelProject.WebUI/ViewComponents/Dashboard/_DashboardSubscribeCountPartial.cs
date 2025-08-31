4a4fb718122d7bc5d38a8190a8dd5b74f1e1ae47
Melek Nur Akdeniz <meleknur_akdeniz@hotmail.com>
2025-08-31 17:47:21 +0300

    Add Instagram follower count feature to dashboard
    This commit introduces a new feature to display Instagram follower and following counts in the dashboard view. The `_DashboardSubscribeCountPartial.cs` file is updated to use an asynchronous method that fetches data from the Instagram API and deserializes it into a new `ResultInstagramFollowersDto` class.
    
    The `Default.cshtml` file is modified to utilize this new model, replacing the previous Google Plus section with an Instagram section. Additionally, the `_ViewImports.cshtml` file is updated to include the `FollowersDto` namespace, and minor styling adjustments are made in `style.css` for social icons.

--- Frontend/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardSubscribeCountPartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardSubscribeCountPartial.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.FollowersDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelProject.WebUI.ViewComponents.Dashboard
 {
     public class _DashboardSubscribeCountPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View();
+            
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://instagram-profile1.p.rapidapi.com/getprofileinfo/skakgun"),
+                Headers =
+    {
+        { "x-rapidapi-key", "b7d12692e3msh1f3ad94cf6bbe2ap10af52jsn8e0f11eabc88" },
+        { "x-rapidapi-host", "instagram-profile1.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                ResultInstagramFollowersDto resultInstagramFollowersDtos = JsonConvert.DeserializeObject<ResultInstagramFollowersDto>(body);
+                return View(resultInstagramFollowersDtos);
+            }
         }
     }
 }
