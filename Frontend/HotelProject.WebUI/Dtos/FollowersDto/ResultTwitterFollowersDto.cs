29789e2fa0ad5bb2f4d93113f40adac9def2b15f
Melek Nur Akdeniz <meleknur_akdeniz@hotmail.com>
2025-08-31 19:36:05 +0300

    Add Twitter API integration for follower statistics
    Updated `_DashboardSubscribeCountPartial.cs` to include an HTTP request to the Twitter API, fetching follower statistics and deserializing the response into a new DTO class, `ResultTwitterFollowersDto`.
    
    Modified `Default.cshtml` to replace hardcoded follower counts with dynamic values from `ViewBag`.
    
    Introduced `ResultTwitterFollowersDto.cs` to structure the data returned from the Twitter API, encapsulating follower and following statistics.

---
+++ Frontend/HotelProject.WebUI/Dtos/FollowersDto/ResultTwitterFollowersDto.cs
@@ -0,0 +1,24 @@
+﻿namespace HotelProject.WebUI.Dtos.FollowersDto
+{
+    public class ResultTwitterFollowersDto
+    {
+
+        public Data? data { get; set; }
+
+
+        public class Data
+        {
+
+            public Stats? stats { get; set; }
+        }
+
+        public class Stats
+        {
+
+            public string? following { get; set; }
+            public string? followers { get; set; }
+
+        }
+
+    }
+}
--- Frontend/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardSubscribeCountPartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardSubscribeCountPartial.cs
@@ -8,7 +8,7 @@ namespace HotelProject.WebUI.ViewComponents.Dashboard
     {
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            
+
             var client = new HttpClient();
             var request = new HttpRequestMessage
             {
@@ -25,10 +25,33 @@ namespace HotelProject.WebUI.ViewComponents.Dashboard
                 response.EnsureSuccessStatusCode();
                 var body = await response.Content.ReadAsStringAsync();
                 ResultInstagramFollowersDto resultInstagramFollowersDtos = JsonConvert.DeserializeObject<ResultInstagramFollowersDto>(body);
-                ViewBag.v1= resultInstagramFollowersDtos.following;
-                ViewBag.v2= resultInstagramFollowersDtos.followers;
-                return View(resultInstagramFollowersDtos);
+                ViewBag.v1 = resultInstagramFollowersDtos.following;
+                ViewBag.v2 = resultInstagramFollowersDtos.followers;
+                //return View(resultInstagramFollowersDtos);
             }
+
+
+            var client2 = new HttpClient();
+            var request2 = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://twitter32.p.rapidapi.com/profile?username=nesoruyonn"),
+                Headers =
+    {
+        { "x-rapidapi-key", "b7d12692e3msh1f3ad94cf6bbe2ap10af52jsn8e0f11eabc88" },
+        { "x-rapidapi-host", "twitter32.p.rapidapi.com" },
+    },
+            };
+            using (var response2 = await client2.SendAsync(request2))
+            {
+                response2.EnsureSuccessStatusCode();
+                var body2 = await response2.Content.ReadAsStringAsync();
+                ResultTwitterFollowersDto resultTwitterFollowersDto = JsonConvert.DeserializeObject<ResultTwitterFollowersDto>(body2);
+                ViewBag.v1 = resultTwitterFollowersDto.data.stats.following;
+                ViewBag.v2 = resultTwitterFollowersDto.data.stats.followers;
+                //return View(resultTwitterFollowersDto);
+            }
+            return View();
         }
     }
 }
