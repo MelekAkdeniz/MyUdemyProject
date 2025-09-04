a0585d3611274e236edf679660feb4c44b81e310
Melek Nur Akdeniz <meleknur_akdeniz@hotmail.com>
2025-09-04 17:16:23 +0300

    Add user settings management feature
    Introduces AdminSettingsController for user profile updates,
    along with UserEditViewModel to encapsulate user data.
    Creates Index.cshtml for user profile editing form and
    updates SidebarPartial.cshtml to link to the new settings page.
    Also modifies _ViewImports.cshtml to include the Setting model.

---
+++ Frontend/HotelProject.WebUI/Controllers/AdminSettingsController.cs
@@ -0,0 +1,28 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using HotelProject.WebUI.Models.Setting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminSettingsController : Controller
+    {
+        private readonly UserManager<AppUser> userManager;
+
+        public AdminSettingsController(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var user= userManager.FindByNameAsync(User.Identity.Name).Result;
+            UserEditViewModel userEditViewModel = new UserEditViewModel();
+            userEditViewModel.Name = user.Name;
+            userEditViewModel.Surname = user.Surname;
+            userEditViewModel.Username = user.UserName;
+            userEditViewModel.Email = user.Email;
+            return View(userEditViewModel);
+        }
+    }
+}
