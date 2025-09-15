3645515e7b9c81e1136590b1a708964e0e1b103d
Melek Nur Akdeniz <meleknur_akdeniz@hotmail.com>
2025-09-15 15:17:48 +0300

    Refactor Twitter follower data handling and UI updates
    - Removed `Data` and `Stats` nested classes from `ResultTwitterFollowersDto`, directly including `followersCount` and `friendsCount`.
    - Updated API endpoint in `_DashboardSubscribeCountPartial.cs` to align with the new structure of `ResultTwitterFollowersDto`.
    - Fixed rendering of the `Name` property in `MessageDetailsByInbox.cshtml`.
    - Changed link in `SidebarPartial.cshtml` from "AdminWorkLocation" to "Login" for logout functionality.
    - Updated button style for "Update" action in `Index.cshtml` from `btn-outline-info` to `btn-outline-success`.
    - Replaced dynamic `ViewBag` values with hardcoded follower and connection counts in `Default.cshtml`, and clarified labels.
    Final.

--- Frontend/HotelProject.WebUI/Dtos/FollowersDto/ResultTwitterFollowersDto.cs
+++ Frontend/HotelProject.WebUI/Dtos/FollowersDto/ResultTwitterFollowersDto.cs
@@ -2,23 +2,8 @@
 {
     public class ResultTwitterFollowersDto
     {
-
-        public Data? data { get; set; }
-
-
-        public class Data
-        {
-
-            public Stats? stats { get; set; }
-        }
-
-        public class Stats
-        {
-
-            public string? following { get; set; }
-            public string? followers { get; set; }
-
-        }
+            public int followersCount { get; set; }
+            public int friendsCount { get; set; }
 
     }
 }
--- Frontend/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardSubscribeCountPartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardSubscribeCountPartial.cs
@@ -28,17 +28,39 @@ namespace HotelProject.WebUI.ViewComponents.Dashboard
                 ViewBag.v1 = resultInstagramFollowersDtos.following;
                 ViewBag.v2 = resultInstagramFollowersDtos.followers;
             }
+           
+
+            //        var client3 = new HttpClient();
+            //        var request3 = new HttpRequestMessage
+            //        {
+            //            Method = HttpMethod.Get,
+            //            RequestUri = new Uri("https://tiktok-api23.p.rapidapi.com/api/user/info?uniqueId=skakgun"),
+            //            Headers =
+            //{
+            //    { "x-rapidapi-key", "b7d12692e3msh1f3ad94cf6bbe2ap10af52jsn8e0f11eabc88" },
+            //    { "x-rapidapi-host", "tiktok-api23.p.rapidapi.com" },
+            //},
+            //        };
+            //        using (var response3 = await client3.SendAsync(request3))
+            //        {
+            //            response3.EnsureSuccessStatusCode();
+            //            var body3 = await response3.Content.ReadAsStringAsync();
+            //            ResultTiktokFollowersDto resultTiktokFollowersDto = JsonConvert.DeserializeObject<ResultTiktokFollowersDto>(body3);
+            //            ViewBag.v5 = resultTiktokFollowersDto.userInfo.stats.followingCount;
+            //            ViewBag.v6 = resultTiktokFollowersDto.userInfo.stats.followerCount;
+            //        }
+            //       
 
 
             var client2 = new HttpClient();
             var request2 = new HttpRequestMessage
             {
                 Method = HttpMethod.Get,
-                RequestUri = new Uri("https://twitter32.p.rapidapi.com/profile?username=nesoruyonn"),
+                RequestUri = new Uri("https://twitter-followers.p.rapidapi.com/nesoruyonn/profile"),
                 Headers =
     {
         { "x-rapidapi-key", "b7d12692e3msh1f3ad94cf6bbe2ap10af52jsn8e0f11eabc88" },
-        { "x-rapidapi-host", "twitter32.p.rapidapi.com" },
+        { "x-rapidapi-host", "twitter-followers.p.rapidapi.com" },
     },
             };
             using (var response2 = await client2.SendAsync(request2))
@@ -46,29 +68,9 @@ namespace HotelProject.WebUI.ViewComponents.Dashboard
                 response2.EnsureSuccessStatusCode();
                 var body2 = await response2.Content.ReadAsStringAsync();
                 ResultTwitterFollowersDto resultTwitterFollowersDto = JsonConvert.DeserializeObject<ResultTwitterFollowersDto>(body2);
-                ViewBag.v3 = resultTwitterFollowersDto.data.stats.following;
-                ViewBag.v4 = resultTwitterFollowersDto.data.stats.followers;
-            }
+                ViewBag.v4 = resultTwitterFollowersDto.followersCount;
+                ViewBag.v3 = resultTwitterFollowersDto.friendsCount;
 
-           
-            var client3 = new HttpClient();
-            var request3 = new HttpRequestMessage
-            {
-                Method = HttpMethod.Get,
-                RequestUri = new Uri("https://tiktok-api23.p.rapidapi.com/api/user/info?uniqueId=skakgun"),
-                Headers =
-    {
-        { "x-rapidapi-key", "b7d12692e3msh1f3ad94cf6bbe2ap10af52jsn8e0f11eabc88" },
-        { "x-rapidapi-host", "tiktok-api23.p.rapidapi.com" },
-    },
-            };
-            using (var response3 = await client3.SendAsync(request3))
-            {
-                response3.EnsureSuccessStatusCode();
-                var body3 = await response3.Content.ReadAsStringAsync();
-                ResultTiktokFollowersDto resultTiktokFollowersDto = JsonConvert.DeserializeObject<ResultTiktokFollowersDto>(body3);
-                ViewBag.v5 = resultTiktokFollowersDto.userInfo.stats.followingCount;
-                ViewBag.v6 = resultTiktokFollowersDto.userInfo.stats.followerCount;
             }
             return View();
         }
